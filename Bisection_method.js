9b3294e837b13f99945bb908f9706a80d7c4b6ce
Hany Medhat <hany.medhat24@gmail.com>
2023-04-23 18:54:03 +0200

    fixed bugs Bisection method

--- Bisection_method.js
+++ Bisection_method.js
@@ -1,26 +1,82 @@
 // const eps = 1.0;
+// import { evaluate, derivative } from "mathjs";
 // const math = require('mathjs');
 // $('#middle').hide();
 $('#btn').click(function () { 
   var formula =$('#equation').val();
-  alert(formula);
+  // alert(formula);
   //  var expression = Parser.parse(formula);
   // alert(expression);
-  function f (x){
-    var scope={ x: Number(x) };
-   var expression= Math.parse(formula);
-   var compiledExpression = expression.compile();
-    return compiledExpression.evaluate(scope);
+  function convert(){
+    if(!formula) return;
+    while(formula.includes("^")){
+      formula = formula.replace("^", "**");
+    }
   }
-  var xl = $('#xl').val();
-  var xu = $('#xu').val();
-  var esp = $('#esp').val();
-  alert((xl)*f(xu));
+  function convertX(expression,X){
+    if(!expression) return;
+    while(expression.includes("x")){
+      expression = expression.replace("x", '*'+Number(X));
+    }
+    return expression;
+  }
+  function f (x){
+    //   var scope={ x: Number(x) };
+    //  var expression= Math.parse(formula);
+    //  var compiledExpression = expression.compile();
+    //   return compiledExpression.evaluate(scope);
+    convert();
+    // alert(formula);
+  var expression= formula;
+  // var compile=expression.replace('x','*'+Number(x));
+  var compile=convertX(expression,x);
+  // alert(compile);
+  return eval(compile);
+  // return expression.eval({x:x});
+  }
+  var xl =parseFloat($('#xl').val());
+  var xu =parseFloat($('#xu').val());
+  var esp =parseFloat($('#esp').val());
+  alert(esp);
+  function bisect(xl,xu,eps=0.1) {
+      var iter =0 ,xr=0,error=0,xrOld=0;
+      // var fxl=[],fxu=[],fxr=[];
+      // var xrOld=[0];
+      do{
+          xrOld=xr;
+          xr=(xl+xu)/2;
+          // alert(xr);
+          if (iter==0) {
+            // alert(xl+" "+xu) ;
+          $('.table').append('<tr><td>'+iter+'</td><td>'+xl+'</td><td>'+f(xl)+'</td><td>'+xu+'</td><td>'+f(xu)+'</td><td>'+xr+'</td><td>'+f(xr)+'</td><td>'+'----'+'</td></tr>');
+          // document.write(iter+'\t'+xl+'\t'+f(xl)+'\t'+xu+'\t'+f(xu)+'\t'+xr+'\t'+f(xr)+'\t'+'----'+'<br>');
+        }
+          else {
+            //  console.log(f(xl));
+              error=Math.abs(((xr-xrOld)/xr)*100);   
+              // alert(xl+" "+xu) ;
+              $('.table').append('<tr><td>'+iter+'</td><td>'+xl+'</td><td>'+f(xl)+'</td><td>'+xu+'</td><td>'+f(xu)+'</td><td>'+xr+'</td><td>'+f(xr)+'</td><td>'+error+'</td></tr>');
+              // document.write(iter+'\t'+xl+'\t'+f(xl)+'\t'+xu+'\t'+f(xu)+'\t'+xr+'\t'+f(xr)+'\t'+error+'<br>');
+          }
+          // fxl[i]=f(xl[iter]);
+          // fxu[i]=f(xu[iter]);
+          // fxr[i]=f(xr[iter]);
+          if (f(xl)*f(xr)>0) {
+              xl=xr;
+          } else {
+              xu=xr;
+          }
+          iter++;
+      }while(error > eps||iter==1);
+    return xr;
+  }
+  // alert(f(xl)*f(xu));
   if (f(xl)*f(xu)>0) {
     alert('No Root in this range');
   } else {
     var root = bisect(xl,xu,esp);
     $('.theRoot').append(root);
+    alert((root).toString);
     // $('#middle').slideDown();
   }
     
@@ -28,28 +84,3 @@ $('#btn').click(function () {
 // function f(x) {
 //     return 4*Math.pow(X,2)-2.3;
 // }
-function bisect(xl,xu,esp=0.1) {
-    var iter =0 ,xr=0,error=0,xrOld=0;
-    // var fxl=[],fxu=[],fxr=[];
-    // var xrOld=[0];
-    do{
-        xrOld=xr;
-        xr=(xl+xu)/2;
-        if (i==0) 
-        $('table').html('<tr><td>'+iter+'</td><td>'+xl+'</td><td>'+f(xl)+'</td><td>'+xu+'</td><td>'+f(xu)+'</td><td>'+xr+'</td><td>'+f(xr)+'</td><td>'+'----'+'</td></tr>');
-         else {
-            error=Math.abs(((xr-xrOld)/xr)*100);    
-            $('table').html('<tr><td>'+iter+'</td><td>'+xl+'</td><td>'+f(xl)+'</td><td>'+xu+'</td><td>'+f(xu)+'</td><td>'+xr+'</td><td>'+f(xr)+'</td><td>'+error+'</td></tr>');
-        }
-        // fxl[i]=f(xl[iter]);
-        // fxu[i]=f(xu[iter]);
-        // fxr[i]=f(xr[iter]);
-        if (fxl(iter)*fxr(iter)>0) {
-            xl=xr(iter);
-        } else {
-            xu=xr(iter);
-        }
-        iter++;
-    }while(error>eps);
-    return xr;
-}
